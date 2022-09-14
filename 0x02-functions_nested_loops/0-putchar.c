046932184bb8e0a2fd570eb5f43ae357dcd32e15
Ronald Okoro <chinoronald93@gmail.com>
2022-09-14 11:44:10 -0700

    good

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print the word "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	int i;
-	char p[] = "Holberton";
+	char p[] = "_putchar";
 
 	for (i = 0; i < 9; i++)
 		{
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
