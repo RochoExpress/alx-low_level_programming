3c0b1b9d49e64d4f415fe246e42842b789eab7f8
Ronald Okoro <chinoronald93@gmail.com>
2022-09-15 21:37:59 -0700

    star

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - entry point
- *
- * Description: prints the numbers 1 - 100 w/
- * Fizz for multiples of 3, Buzz for multiples of 5,
- * and FizzBuzz for multiples of both
- *
- * Return: always 0
- */
-int main(void)
+* print_triangle - prints a triangle
+* @size: takes in a integer for size of triangle
+*/
+void print_triangle(int size)
 {
-	int i;
+	int row, column, k;
 
-	for (i = 1; i <= 100; i++)
+	if (size <= 0)
+		_putchar('\n');
+	for (row = 0; row < size; row++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
+		for (column = size - row; column > 1; column--)
+			_putchar(' ');
+		for (k = row + column; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
 	}
-	printf("\n");
-	return (0);
 }
