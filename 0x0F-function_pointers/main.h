82e20167c8f0940d66b164beff7d01e1c4bfdb64
Ronald Okoro <chinoronald93@gmail.com>
2022-10-12 20:50:41 -0700

    correct

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,11 +1,10 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+#ifndef ASSIGN_0x0F
+#define ASSIGN_0x0F
 
 #include <stddef.h>
-
-int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
 
-#endif /* _FUNCTION_POINTERS_H_ */
+#endif
