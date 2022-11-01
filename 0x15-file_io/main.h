ddaeac40e19f438eebfc26bd668bd3c2335b4569
Ronald Okoro <chinoronald93@gmail.com>
2022-10-31 19:30:17 -0700

    save

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,14 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef HEADER_H
+#define HEADER_H
 
-int _putchar(char c);
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUFSIZE 1204
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-
+int _putchar(char c);
 #endif
